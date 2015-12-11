72646b515cda740d7bc43e3edcf7885a206fc16b
Nichole Boseman <brandi.boseman@nhfc.com>
2015-12-11 12:11:42 -0500

    updated calss

--- routes/calls.js
+++ routes/calls.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('Call log');
+	var db = req.db;
+	var calls = db.get('calls');
+	
+	calls.find( {}, {}, function(err, calls) {
+		res.render('calls/index', {
+			"calls":calls
+		});
+	});
 });
 
 module.exports = router;
